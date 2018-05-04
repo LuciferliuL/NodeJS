df8ed64e8734f48107220ec7a54897e842d09e9a
luciferL <luciferlikeme@163.com>
2018-05-04 20:25:20 +0800

    调用

--- ensample/HelloWorld.js
+++ ensample/HelloWorld.js
@@ -1,3 +1,11 @@
 'use strict'
 console.log('HelloWorld');
 console.log(100+200+300);
+
+var s = 'HelloWorld';
+
+function greet(name){
+    console.log(s + ',' + name + '!')
+}
+
+module.exports = greet
---
+++ ensample/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var greet = require('./HelloWorld');
+
+var s = 'Michael';
+
+greet(s);
