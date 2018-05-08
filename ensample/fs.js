06c949ada6c5036bf77988f41a5b69982e643c95
luciferL <luciferlikeme@163.com>
2018-05-08 08:57:24 +0800

    try..catch

--- ensample/fs.js
+++ ensample/fs.js
@@ -3,13 +3,20 @@
 var fs = require('fs');
 
 //读取正确 err为null，data为读取的string，读取错误err为错误对象，data为undefined
-fs.readFile('./sample.txt','utf-8',(err,data)=>{
-    if(err){
+fs.readFile('./sample.txt', 'utf-8', (err, data) => {
+    if (err) {
         console.log(err)
-    }else{
+    } else {
         console.log(data)
     }
 })
 //同步读取文件没有回调
-var data = fs.readFileSync('./sample.txt','utf-8');
+var data = fs.readFileSync('./sample.txt', 'utf-8');
 console.log(data)
+//读取错误
+try {
+    var data = fs.readFileSync('./sample.txt', 'utf-8');
+    console.log(data)
+} catch (err) {
+    //出错
+}
